4c8b001eabe9b625a1117b659b4d8f4a22a2aa02
AbdullahVC <69648705+AbdullahVC@users.noreply.github.com>
2024-08-19 20:31:54 +0300

    Add project files.

---
+++ Encapsulation/Araba.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Araba
+    {
+        private int _kapıSayisi;
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+
+        public int KapıSayisi
+        {
+            get { return _kapıSayisi; }
+            set
+            {
+                // Eğer kapı sayısı 2 veya 4 değilse, uyarı ver ve -1 olarak ayarla
+                if (value == 2 || value == 4)
+                {
+                    _kapıSayisi = value;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz Kapı Sayısı!");
+                    _kapıSayisi = -1;
+                }
+            }
+        }
+
+        // Constructor
+        public Araba(string marka, string model, string renk, int kapıSayisi)
+        {
+            Marka = marka;
+            Model = model;
+            Renk = renk;
+            KapıSayisi = kapıSayisi; 
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine($"Marka: {Marka}, Model: {Model}, Renk: {Renk}, Kapı Sayısı: {KapıSayisi}");
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba("Toyota", "Corolla", "Beyaz", 4);
+            araba1.BilgileriGoster();
+
+            Araba araba2 = new Araba("Honda", "Civic", "Siyah", 3);
+            araba2.BilgileriGoster();
+        }
+    }
+}
